fe15b4bc262bc3253518d96ef4adec72f7a73613
George Gabriel <geogab.dev@gmail.com>
2026-05-28 10:20:36 -0300

    Adjust re-export data contracts modules

--- src/core/contracts/__init__.py
+++ src/core/contracts/__init__.py
@@ -1,3 +1,20 @@
 from .bronze import BRONZE_EVENTS_SCHEMA, BronzeEventsContract
+from .silver import (
+    KNOWN_ACTIONS,
+    KNOWN_EVENT_TYPES,
+    ActorsContract,
+    OrgsContract,
+    ReposContract,
+    SilverEventsContract,
+)
 
-__all__: list[str] = ["BRONZE_EVENTS_SCHEMA", "BronzeEventsContract"]
+__all__: list[str] = [
+    "BRONZE_EVENTS_SCHEMA",
+    "BronzeEventsContract",
+    "KNOWN_ACTIONS",
+    "KNOWN_EVENT_TYPES",
+    "ActorsContract",
+    "OrgsContract",
+    "ReposContract",
+    "SilverEventsContract",
+]
